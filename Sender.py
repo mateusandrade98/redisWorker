859f5b20b9f644aa28f6e2efae663721eff8e816
Mateus Andrade <38530760+joandesonandrade@users.noreply.github.com>
2022-05-07 21:18:55 -0300

    new200

--- Sender.py
+++ Sender.py
@@ -1,34 +1,46 @@
 import requests
-import json
+
 
 class Request:
     def __init__(self):
         self.data = None
 
-    def run(self, data=dict):
+    async def run(self, data: dict = None):
         self.data = data
         if self.data:
             url = self.data["url"]
+
+            if "redirect" in self.data:
+                requests.post(
+                    url=url,
+                    json=self.data
+                )
+
+                return {"success": 1}
+
             try:
                 payload = self.data["payload"]
             except KeyError:
                 payload = {}
+
             try:
                 headers = self.data["headers"]
                 values_headers = headers.items()
                 headers = {str(key): str(value) for key, value in values_headers}  # convert key:value to string
             except KeyError:
                 headers = {}
+
             try:
                 method = self.data["method"]
             except KeyError:
                 method = "GET"
+
             try:
                 params = self.data["params"]
             except KeyError:
                 params = {}
 
-            response = requests.request(
+            requests.request(
                 method=method,
                 url=url,
                 headers=headers,
@@ -36,7 +48,4 @@ class Request:
                 params=params
             )
 
-            try:
-                return response.json()
-            except json.JSONDecodeError:
-                return response.text
+            return {"success": 1}
